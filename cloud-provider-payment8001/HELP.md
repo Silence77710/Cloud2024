5bd6fe5c716645d2c8b32f1bd1572e990a7acf8c
Slience77710 <1448412458@qq.com>
2024-06-30 22:49:17 +0800

    第一个支付模块的框架创建

---
+++ cloud-provider-payment8001/HELP.md
@@ -0,0 +1,4 @@
+#### 构建的第一个支付模块
+
+@MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+这样可以让所有的mapper不再需要添加@Mapper注解
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/CloudProviderPayment8001Application.java
@@ -0,0 +1,15 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+public class CloudProviderPayment8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8001Application.class, args);
+    }
+
+}
